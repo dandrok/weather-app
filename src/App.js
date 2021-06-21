7b3e9fc9063480c9fda78a4a8a08129adbd089e7
dandrok <patryk.stusik@gmail.com>
2021-06-21 20:24:49 +0200

    check location and add fetch from api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import Search from './components/Search'
 import Info from './components/Info'
 import Temp from './components/Temp'
+import styles from './_App.module.scss'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [lat, setLat] = useState([])
+  const [long, setLong] = useState([])
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude)
+        setLong(position.coords.longitude)
+      })
+
+      await fetch(
+        `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result)
+          console.log(result)
+        })
+    }
+
+    fetchData()
+  }, [lat, long])
+
   return (
-    <div>
-      <section>
-        <Search />
-        <Info />
-        <Temp />
+    <div className={styles.widthmin}>
+      <section className={styles.container}>
+        {typeof data.main != 'undefined' ? (
+          <Info weatherData={data} />
+        ) : (
+          <div></div>
+        )}
       </section>
     </div>
   )
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import styles from './_Info.module.scss'
 
-const Info = () => {
-  return <div>Info</div>
+const Info = ({ weatherData }) => {
+  const temp = weatherData.main.temp
+  return (
+    <section>
+      <div className={styles.test}>{weatherData.name}</div>
+      <div className={styles.test}>{weatherData.weather[0].description}</div>
+      <div className={styles.test}>temp: {Math.round(temp)} &#8451;</div>
+    </section>
+  )
 }
 
 export default Info
